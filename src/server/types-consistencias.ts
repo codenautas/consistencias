08033ac18cf46b134d93b719783d2cbff56cf1c3
Eugenio Arosteguy <eugenio.arosteguy@gmail.com>
2018-11-09 16:56:04 -0300

    avance en compilación de consistencias

--- src/server/types-consistencias.ts
+++ src/server/types-consistencias.ts
@@ -1,8 +1,8 @@
 import * as EP from "expre-parser";
-import { OperativoGenerator, Variable } from 'operativos';
+import { OperativoGenerator, Variable, prefijarExpresion, getWrappedExpression, compilerOptions } from 'varcal';
 import { Client } from 'pg-promise-strict';
 
-export * from 'operativos';
+export * from 'varcal';
 
 export abstract class ConsistenciaDB {
     operativo: string
@@ -30,6 +30,7 @@ export class Consistencia extends ConsistenciaDB {
     insumosVars: Variable[];
     client: Client;
     condInsumos: EP.Insumos;
+    opGen: OperativoGenerator;
 
     static async fetchOne(client: Client, op: string, con: string): Promise<Consistencia> {
         let result = await client.query(`SELECT * FROM consistencias c WHERE c.operativo = $1 AND c.con = $2`, [op, con]).fetchUniqueRow();
@@ -54,29 +55,33 @@ export class Consistencia extends ConsistenciaDB {
 
     //chequear que la expresiones (pre y post) sea correcta (corriendo un select simple para ver si falla postgres) 
     async validateCondSql() {
-        let insumosTDs:string[] = this.insumosVars.map(v=>v.tabla_datos);
+        let insumosTDNames:string[] = this.insumosVars.map(v=>v.tabla_datos);
         
-        // insumosTDs = ['grupo_personas', 'personas', 'personas_calculadas']
-        // let relaciones = Relaciones.fetchAll(this.client);
+        // TODO: ORDENAR:
+        // primero: la td que no tenga ninguna TD en que busco es la principal
+        // segundas: van todas las tds que tengan en "que_busco" a la principal
+        // terceras: las tds que tengan en "que busco" a las segundas
+        insumosTDNames = ['grupo_personas', 'grupo_personas_calculada', 'personas', 'personas_calculadas']
+        let mainTD = this.opGen.getTD(insumosTDNames[0]);
+        let lastTD = this.opGen.getTD(insumosTDNames[insumosTDNames.length-1]);
+        this.clausula_from = 'FROM '+ mainTD.getTableName();
 
-        this.clausula_from = 'FROM '+ insumosTDs.shift() + insumosTDs.map(tdName=> {
-            ` JOIN ${tdName} ON `
-        });
+        for(let i=1; i < insumosTDNames.length; i++){
+            let leftInsumoTDName = insumosTDNames[i-1];
+            let rightInsumoTDName = insumosTDNames[i];
+            this.clausula_from += this.opGen.joinTDs(leftInsumoTDName, rightInsumoTDName);
+        }
+        
+        this.precondicion = getWrappedExpression(this.precondicion, lastTD.getPKCSV(), compilerOptions);
+        this.postcondicion = getWrappedExpression(this.postcondicion, lastTD.getPKCSV(), compilerOptions);
 
-        insumosTDs.forEach(tdName => {
-            this.clausula_from += ` ${tdName} ` 
-        });
+        prefijarExpresion(this.precondicion, EP.parse(this.precondicion).getInsumos(), this.opGen.myVars)
+        prefijarExpresion(this.postcondicion, EP.parse(this.postcondicion).getInsumos(), this.opGen.myVars)
 
-        // prefijar todas las variables insumos
-        // pasar wrapper de expreparser a ambas condiciones
-
-        this.clausula_from = `FROM grupo_personas gp 
-                                JOIN personas p ON gp.operativo = p.operativo AND gp.id_caso = p.id_caso 
-                                JOIN personas_calculada pc ON p.operativo = pc.operativo AND p.id_caso = pc.id_caso AND p.p0 = pc.p0`;
         this.clausula_where = `WHERE (${this.precondicion}) AND (${this.postcondicion}) IS NOT TRUE`;
         let selectQuery = `SELECT true 
-                            ${this.clausula_from} 
-                            ${this.clausula_where} 
+                            ${this.clausula_from}
+                            ${this.clausula_where}
                             limit 1`;
         await this.client.query(selectQuery, []).fetchOneRowIfExists;
     }
@@ -89,12 +94,12 @@ export class Consistencia extends ConsistenciaDB {
     }
 
     throwError(specificError: string) {
-        throw new Error(`La consistencia "${this.con}" del operativo ${OperativoGenerator.instanceObj.operativo} es inválida. ` + specificError);
+        throw new Error(`La consistencia "${this.con}" del operativo ${this.opGen.operativo} es inválida. ` + specificError);
     }
 
     //TODO: ADD PREFIJOS!!
     validateVars(varNames: string[]): void {
-        let operativoGenerator = OperativoGenerator.instanceObj;
+        let operativoGenerator = this.opGen;
         varNames.forEach(varName => {
             let varsFound = operativoGenerator.myVars.filter(v => v.variable == varName);
             if (varsFound.length > 1) {
@@ -114,7 +119,9 @@ export class Consistencia extends ConsistenciaDB {
     }
 
     validateFunctions(funcNames: string[]) {
-        let whiteList = ['div', 'avg', 'count', 'max', 'min', 'sum', 'coalesce'];
+        let pgWitheList = ['div', 'avg', 'count', 'max', 'min', 'sum', 'coalesce'];
+        let userWhiteList = ['informado', 'dic_tradu'];
+        let whiteList = pgWitheList.concat(userWhiteList);
         funcNames.forEach(f => {
             if (whiteList.indexOf(f) == -1) {
                 throw new Error('La Función ' + f + ' no está incluida en la whiteList de funciones: ' + whiteList.toString());
@@ -139,6 +146,7 @@ export class Consistencia extends ConsistenciaDB {
     // responsabilidades: chequear que sea valida y generar el sql 
     async compilar(client: Client) {
         this.client = client;
+        this.opGen = OperativoGenerator.instanceObj;
         //TODO: cuando se compile en masa sacar este fetchall a una clase Compilador que lo haga una sola vez
         try {
             await this.validate();
@@ -146,6 +154,7 @@ export class Consistencia extends ConsistenciaDB {
         } catch (error) {
             this.cleanDB();
             this.error_compilacion = (<Error>error).message;
+            // TODO agregar error genérico de consistencia que falló aún cuando falle por postgres (en la ejecución del select)
             throw error;
         }finally{
             await this.save();
