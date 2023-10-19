a108483a83351d6b79d26407e997fbc5a512ff0e
Eugenio Arosteguy <eugenio.arosteguy@gmail.com>
2023-10-19 17:55:42 -0300

    gestik#63 INCORPORAR EN LAS GRILLAS DE INCONSISTENCIAS

--- src/server/table-inconsistencias.ts
+++ src/server/table-inconsistencias.ts
@@ -2,9 +2,9 @@
 import {TableContext,TableDefinition} from "varcal"
 
 export function inconsistencias(context:TableContext):TableDefinition{
-    var isAdmin=context.user.rol==='admin';
-    var isProcesamiento=context.user.rol==='procesamiento' || isAdmin;
-    return {
+    var isAdmin = context.user.rol === 'admin';
+    var isProcesamiento = context.user.rol === 'procesamiento' || isAdmin;
+    var def = {
         name: 'inconsistencias',
         elementName: 'inconsistencia',
         editable: isProcesamiento,
@@ -17,20 +17,39 @@ export function inconsistencias(context:TableContext):TableDefinition{
             { name: "autor_justificacion"  , typeName: "text"    },
             { name: "observacion"          , typeName: "text"    },
             { name: "corrida"              , typeName: "timestamp" ,editable: false, visible: false},
-            { name: "vigente"              , typeName: "boolean"   ,editable: false, visible: false},
+            { name: "vigente"              , typeName: "boolean"   ,editable: false, inTable: false},
             { name: "incon_valores"        , typeName: "jsonb"     , editable: false},
+            
+            { name: "momento", typeName: "text", inTable: false    },
+
         ],
         primaryKey: ['operativo', 'pk_integrada', 'consistencia'],
-        foreignKeys:[
-            {references:'operativos', fields:['operativo']},
-            {references:'consistencias', fields:['operativo', 'consistencia']},
+        foreignKeys: [
+            { references: 'operativos', fields: ['operativo'] },
+            { references: 'consistencias', fields: ['operativo', 'consistencia'] },
         ],
         sql:{
-            where:"consistencias.activa and consistencias.valida"
+            isTable:true,
+                from:`(
+                    SELECT i.*, (c.activa AND c.valida) as vigente, c.momento
+                    FROM inconsistencias i 
+                        LEFT JOIN consistencias c USING (operativo, consistencia)
+                )`,
+            // where:"consistencias.activa AND consistencias.valida"
         }
         // detailTables: [
         //     { table: 'in_con_var', fields: ['operativo', 'consistencia', 'pk_integrada'], abr: 'cv', label: 'in con vars' }
         // ]
-    }
+    };
+    def.fields=[
+        def.fields[0],
+        def.fields[1],
+        {name: "comp",  typeName: 'text',  clientSide:'verCaso', editable: false },
+        {name: "infe", typeName: 'text', clientSide:'verViejo', editable: false },
+        {name: "cod_causa", typeName: 'text', clientSide:'verCausa', editable: false },
+        {name: "domi", typeName: 'text', clientSide:'verDomicilio', editable: false },
+        ...def.fields.slice(2),
+    ];
+    return def;
 }
 
